d4d0b3c62ab86238d473f0b8cc53d00ec694d6cf
Ryiki <113627503+Ryiki@users.noreply.github.com>
2024-03-21 18:35:22 +0100

    Elérési útvonalak és fájl módosítások
    Módosítottam a grafikai elemek elérési útvonalát, hogy tükrözze az éles szerveren használt útvonalat, illetve az index fájlnak mostantól php a kiterjesztése, valamint linkeltem hozzá a Bootstrap v5.3.3-at így használatra kész.

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Mineband</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="top-bar">
+            <div class="menu-bar">
+                <button class="menu-button">menu1</button>
+                <button class="menu-button">menu2</button>
+                <button class="menu-button">menu3</button>
+            </div>
+
+            <div class="main-logo">
+                <img src="./Resources/Logos/mb_shiro_logo.png" alt="">
+            </div>
+        </div>
+
+        <div class="content">
+
+            
+            <div class="news">
+
+                <div class="video-list">
+                    <iframe src="https://www.youtube.com/embed/jfKfPfyJRdk?si=fWixI1XwQk-dNXcU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/_HjRyDRW-V0?si=TqqylVwwpEscIzLF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/_HjRyDRW-V0?si=TqqylVwwpEscIzLF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe src="https://www.youtube.com/embed/_HjRyDRW-V0?si=TqqylVwwpEscIzLF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
+                <div class="news-block">
+                    <div class="news-picture"><img src="./Resources/Templates/post_template_3.png"></div>
+                    <div class="news-textbox">
+                        <p class="news-date">2024-03-16</p>
+                        <p class="news-author">írta:Ferkó</p>
+                        <h1 class="news-title">Teszt üzenet</h1>
+                        <p class="news-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia earum, laborum odit necessitatibus repudiandae modi nobis, soluta delectus est doloremque rerum, sunt ratione saepe quaerat reprehenderit quam sed recusandae quisquam?</p>
+                        <p class="news-char">(még 14321 karakter)</p>
+                        <button class="news-button">Tovább</button>
+                    </div>
+                </div>
+                
+                
+                <div class="news-block">
+                    <div class="news-picture"><img src="./Resources/Templates/post_template_4.jpg"></div>
+                    <div class="news-textbox">
+                        <p class="news-date">2024-03-12</p>
+                        <p class="news-author">írta:Ferkó</p>
+                        <h1 class="news-title">Vicces hír</h1>
+                        <p class="news-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia earum, laborum odit necessitatibus repudiandae modi nobis, soluta delectus est doloremque rerum, sunt ratione saepe quaerat reprehenderit quam sed recusandae quisquam?</p>
+                        <p class="news-char">(még 1432 karakter)</p>
+                        <button class="news-button">Tovább</button>
+                    </div>
+                </div>
+                
+                <div class="news-block">
+                    <div class="news-picture"><img src="./Resources/Templates/post_template_5.jpg"></div>
+                    <div class="news-textbox">
+                        <p class="news-date">2024-03-12</p>
+                        <p class="news-author">írta:Ferkó</p>
+                        <h1 class="news-title">Teszt 0.0</h1>
+                        <p class="news-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia earum, laborum odit necessitatibus repudiandae modi nobis, soluta delectus est doloremque rerum, sunt ratione saepe quaerat reprehenderit quam sed recusandae quisquam?</p>
+                        <p class="news-char">(még 121 karakter)</p>
+                        <button class="news-button">Tovább</button>
+                    </div>
+                </div> 
+
+            </div>
+
+            <div class="sidebar">
+                <div class="login-box">
+                    <a href="">
+                        <img src="./Resources/Buttons/login_button.png" alt="">
+                        <h2>Bejelentkezés</h2>
+                    </a>
+                </div>
+                <div class="logos">
+                    <img src="./Resources/Logos/nerik_logo.png" alt="">
+                    <img src="./Resources/Logos/hawk_logo.png" alt="">
+                    <img src="./Resources/Logos/mb_shiro_logo.png" alt="">
+                </div>
+                <div class="most-popular-articles" >
+                    <h3>Legolvasottabb cikkek</h3>
+                    <p>valami - 2123 megtekintés</p>
+                    <p>valami - 2123 megtekintés</p>
+                    <p>valami - 2123 megtekintés</ps>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
