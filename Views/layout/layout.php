08ab5c160cb1401e3755bd7bdffea368ed987a0a
Ryiki <113627503+Ryiki@users.noreply.github.com>
2024-03-24 18:17:12 +0100

    Projekt munka váltás
    Kivettem a képi elemeket és annak elérési útvonalait. Így egyenlőre nagyon le van csupaszítva. Megbeszéltek szerint akkor ha feri kiválasztotta a szervezetet, úgy módosítjuk.

--- Views/layout/layout.php
+++ Views/layout/layout.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
-    <title>Mineband</title>
+    <title>Csábító Tea Party</title>
 </head>
 
 <body>
@@ -16,25 +16,18 @@
             <div class="col-9 menu-area">
                 <div class="row menu-bar">
                     <div class="col">
-                        <a href=""><img class="img-fluid" src="./Resources/Buttons/house_icon.png" alt=""></a>
-                        <p>home</p>
+                        <p>Home</p>
                     </div>
                     <div class="col">
-                        <a href=""><img class="img-fluid" src="./Resources/Buttons/pictures_icon.png" alt=""></a>
-                        <p>pictures</p>
+                        <p>Videos</p>
                     </div>
                     <div class="col">
-                        <a href=""><img class="img-fluid" src="./Resources/Buttons/videos_icon.png" alt=""></a>
-                        <p>videos</p>
+                        <p>Pictures</p>
                     </div>
                     <div class="col">
-                        <a href=""><img class="img-fluid" src="./Resources/Buttons/aboutUs_icon.png" alt=""></a>
-                        <p>among us</p>
+                        <p>Login</p>
                     </div>
                 </div>
-            </div>
-            <div class="col-3 main-logo">
-                <a href=""><img class="img-fluid" src="./Resources/Logos/mb_logo.png"></a>
             </div>
         </div>
         <div class="row content-area">
@@ -44,9 +37,6 @@
 
 
             <div class="col-3 side-bar">
-                <div class="row"><a href="">
-                        <img src="Resources/Buttons/login_button.png" alt="">
-                    </a></div>
                 <div class="row">
                     <form action="sendEmail.php" method="post">
                         <label for="email">Enter your email:</label>
