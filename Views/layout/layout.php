8e3b816f2e0156f881f3950e95579ee79f643acd
Szabo Ferenc <irkm813@gmail.com>
2024-03-27 23:20:59 +0100

    Adjustments in html and css to make the site more readable on mobile devices

--- Views/layout/layout.php
+++ Views/layout/layout.php
@@ -11,11 +11,13 @@
 
 <body>
     <div class="container all-content">
-
         <div class="row top-bar">
-            <div class="col-9 menu-area">
+            <div class="col-12 col-md-9 menu-area">
                 <div class="row menu-bar">
                     <div class="col">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+                        </svg>
                         <p>Home</p>
                     </div>
                     <div class="col">
@@ -31,23 +33,19 @@
             </div>
         </div>
         <div class="row content-area">
-            <div class="col-9 news-column">
+            <div class="col-12 col-md-9 news-column">
                 {{content}}
             </div>
 
 
-            <div class="col-3 side-bar">
+            <div class="col-3 d-none d-md-block side-bar">
                 <div class="row">
-                    <form action="sendEmail.php" method="post">
-                        <label for="email">Enter your email:</label>
-                        <input type="email" id="email" name="email">
-                        <button type="submit">Send Email</button>
-                    </form>
+                    <h1>valami</h1>
                 </div>
                 <div class="row">
-                    <p class="rot30">Zsolti</p>
-                    <p class="rot45">Erik</p>
-                    <p class="rot83">Feri</p>
+                    <p>Zsolti</p>
+                    <p>Erik</p>
+                    <p>Feri</p>
                 </div>
             </div>
         </div>
