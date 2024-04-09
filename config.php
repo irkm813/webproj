8ddff82d9067339960315478724d32f111e0bdf4
Szabo Ferenc <irkm813@gmail.com>
2024-04-09 21:55:08 +0200

    semmi

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_HOST","");
 define("DB_USER","");
-define("DB_PASSWORD","");
+define("DB_PASSWORD","megegyteszt");
 define("DB_DATABASE","nemtomvalami");
 define("MAX_FILESIZE", 5*1048576);
 
